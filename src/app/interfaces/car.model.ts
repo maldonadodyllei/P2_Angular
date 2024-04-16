0c618137b928ee1fbc749f1831357e5d4e96b1f3
Dylan Anderson Leiva Maldonado <100689619+maldonadodyllei@users.noreply.github.com>
2024-04-16 19:58:22 +0200

    Creacion page cars - revisar

---
+++ src/app/interfaces/car.model.ts
@@ -0,0 +1,11 @@
+export interface Car {
+    brand: string;
+    model: string;
+    category: string;
+    image: string;
+    cv: number;
+    maxSpeed: number;
+    accelerationTime: number;
+    price: number;
+    available: boolean;
+}
--- src/app/modules/cars/cars.component.ts
+++ src/app/modules/cars/cars.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CardCarComponent } from './card-car/card-car.component';
+import { Car } from '../../interfaces/car.model';
+import { CarsService } from '../../services/cars.service';
 @Component({
   selector: 'app-cars',
   standalone: true,
-  imports: [],
+  imports: [CardCarComponent],
   templateUrl: './cars.component.html',
   styleUrl: './cars.component.css'
 })
-export class CarsComponent {
+export class CarsComponent implements OnInit{
+  carList: Car[] = [];
+  constructor(
+    public carsService: CarsService
+  ) { 
+    this.carsService.carList$.subscribe((cars: Car[]) => {
+      this.carList = cars;
+    });
+  }
 
+  ngOnInit(): void {
+  }
 }
